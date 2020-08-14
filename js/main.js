a3a1933614c0993d2eb0bc41778de5743c8474cb
João Vítor <joaovitorsf7@hotmail.com>
2020-08-14 21:08:15 +0000

    começo do desenvolvimento do JS.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let keys = document.querySelector(".container").childNodes;
+
+function playNote (event) {
+    // console.log(event);
+    // console.log(keys);
+}
+
+
+window.addEventListener("keydown", playNote);
