906ecb2763cc8dfd03f859077ecada0dbf77c01c
João Vítor <joaovitorsf7@hotmail.com>
2020-08-15 22:25:54 +0000

    lógica do clique do mouse na nota específica.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,33 @@ let keys = document.querySelector(".container").childNodes;
 
 
 function playNote (event) {
-    // console.log(event);
+    // dó
+    if (event.target == keys[1]) {
+        console.log(event.target);
+    }
+    // ré
+    else if (event.target == keys[3]) {
+        console.log(event.target);
+    }
+    // mi
+    else if (event.target == keys[5]) {
+        console.log(event.target);
+    }
+    // fá
+    else if (event.target == keys[7]) {
+        console.log(event.target);
+    }
+    //sol
+    else if (event.target == keys[9]) {
+        console.log(event.target);
+    }
+    //lá
+    else if (event.target == keys[11]) {
+        console.log(event.target);
+    }
+    else if (event.target == keys[13]) {
+        console.log(event.target);
+    }
 }
 
 
