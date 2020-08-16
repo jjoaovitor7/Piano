bed6ae5035455634fea883ca4f481328932abc30
João Vítor <joaovitorsf7@hotmail.com>
2020-08-16 14:35:40 +0000

    som quando clica na tecla do teclado correspondente a nota.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 let keys       = document.querySelector(".container").childNodes;
 let keysAudios = document.querySelectorAll("audio");
 // console.log(keys);
-console.log(keysAudios);
+// console.log(keysAudios);
 
 
 function playNote (event) {
+    //mouse
     // dó
     if (event.target == keys[1]) {
         keysAudios[0].play();
@@ -32,6 +33,46 @@ function playNote (event) {
     }
     else if (event.target == keys[13]) {
         keysAudios[6].play();
+    }
+
+
+    //teclado
+    // dó
+    if (event.key == "c") {
+        keysAudios[0].play();
+    }
+
+    // ré
+    else if (event.key == "d") {
+        keysAudios[1].play();
+    }
+
+    // mi
+    else if (event.key == "e") {
+        keysAudios[2].play();
+    }
+
+    // fá
+    else if (event.key == "f") {
+        keysAudios[3].play();
+    }
+
+    // sol
+    else if (event.key == "g") {
+        keysAudios[4].play();
+    }
+
+    // lá
+    else if (event.key == "a") {
+        keysAudios[5].play();
+    }
+
+    // si
+    else if (event.key == "b") {
+        keysAudios[6].play();
+    }
+    else {
+        return 0;
     }
 }
 
