9597da9a0638c360ed3d67bd59ef9f713f9d415b
João Vítor <joaovitorsf7@hotmail.com>
2021-01-08 00:03:07 -0300

    remoção dos áudios do pinkyfinger. adição de mais notas. utilização da biblioteca simpleTones.js. inclusão de licença do projeto simpleTones.js.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,81 +1,118 @@
 // notas
-let keys       = document.querySelector(".container").childNodes;
-let keysAudios = document.querySelectorAll("audio");
-// console.log(keys);
-// console.log(keysAudios);
+const mainKeys = document.querySelectorAll(".main-note");
+const subKeys = document.querySelectorAll(".sub-note");
 
+function playNote(event) {
+  //mouse
+  // dó
+  if (event.target == mainKeys[0]) {
+    playTone(261, "sine", 0.5);
+  }
 
-function playNote (event) {
-    //mouse
-    // dó
-    if (event.target == keys[1]) {
-        keysAudios[0].play();
-    }
-    // ré
-    else if (event.target == keys[3]) {
-        keysAudios[1].play();
-    }
-    // mi
-    else if (event.target == keys[5]) {
-        keysAudios[2].play();
-    }
-    // fá
-    else if (event.target == keys[7]) {
-        keysAudios[3].play();
-    }
-    //sol
-    else if (event.target == keys[9]) {
-        keysAudios[4].play();
-    }
-    //lá
-    else if (event.target == keys[11]) {
-        keysAudios[5].play();
-    }
-    else if (event.target == keys[13]) {
-        keysAudios[6].play();
-    }
+  // dó sustenido
+  else if (event.target == subKeys[0]) {
+    playTone(277.2, "sine", 0.5);
+  }
 
+  // ré
+  else if (event.target == mainKeys[1]) {
+    playTone(293.7, "sine", 0.5);
+  }
 
-    //teclado
-    // dó
-    if (event.key == "c") {
-        keysAudios[0].play();
-    }
+  // ré sustenido
+  else if (event.target == subKeys[1]) {
+    playTone(311.1, "sine", 0.5);
+  }
 
-    // ré
-    else if (event.key == "d") {
-        keysAudios[1].play();
-    }
+  // mi
+  else if (event.target == mainKeys[2]) {
+    playTone(329.6, "sine", 0.5);
+  }
 
-    // mi
-    else if (event.key == "e") {
-        keysAudios[2].play();
-    }
+  // fá
+  else if (event.target == mainKeys[3]) {
+    playTone(349.2, "sine", 0.5);
+  }
 
-    // fá
-    else if (event.key == "f") {
-        keysAudios[3].play();
-    }
+  // fá sustenido
+  else if (event.target == subKeys[2]) {
+    playTone(370.0, "sine", 0.5);
+  }
 
-    // sol
-    else if (event.key == "g") {
-        keysAudios[4].play();
-    }
+  // sol
+  else if (event.target == mainKeys[4]) {
+    playTone(392.0, "sine", 0.5);
+  }
 
-    // lá
-    else if (event.key == "a") {
-        keysAudios[5].play();
-    }
+  // sol sustenido
+  else if (event.target == subKeys[3]) {
+    playTone(466.2, "sine", 0.5);
+  }
 
-    // si
-    else if (event.key == "b") {
-        keysAudios[6].play();
-    }
-    else {
-        return 0;
-    }
+  // lá
+  else if (event.target == mainKeys[5]) {
+    playTone(440.0, "sine", 0.5);
+  }
+
+  // lá sustenido
+  else if (event.target == subKeys[4]) {
+    playTone(466.2, "sine", 0.5);
+  }
+
+  // si
+  else if (event.target == mainKeys[6]) {
+    playTone(493.9, "sine", 0.5);
+  }
+
+  //   //teclado
+  //   // dó
+  //   if (event.key == "c") {
+  //     playTone(261, "sine", 0.5);
+  //   }
+
+  //   // ré
+  //   else if (event.key == "d") {
+  //     playTone(293.7, "sine", 0.5);
+  //   }
+
+  //   // mi
+  //   else if (event.key == "e") {
+  //     playTone(329.6, "sine", 0.5);
+  //   }
+
+  //   // fá
+  //   else if (event.key == "f") {
+  //     playTone(349.2, "sine", 0.5);
+  //   }
+
+  //   // sol
+  //   else if (event.key == "g") {
+  //     playTone(392.0, "sine", 0.5);
+  //   }
+
+  //   // lá
+  //   else if (event.key == "a") {
+  //     playTone(440.0, "sine", 0.5);
+  //   }
+
+  //   // si
+  //   else if (event.key == "b") {
+  //     playTone(493.9, "sine", 0.5);
+  //   }
+  else {
+    return 0;
+  }
 }
 
+//clique do mouse
+for (let key of mainKeys) {
+  key.addEventListener("click", playNote);
+}
 
-// clique do teclado
-window.addEventListener("keydown", playNote);
+//clique do mouse
+for (let key of subKeys) {
+  key.addEventListener("click", playNote);
+}
+
+// // clique do teclado
+// window.addEventListener("keydown", playNote);
