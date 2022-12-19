928299ed43e39da4447c2085c1b353675d0ed2ee
João Vítor <joaovitorsf7@hotmail.com>
2022-12-19 17:45:22 -0300

    refatoração e remoção do simpleTones.js.

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Piano
-[![ForTheBadge uses-html](http://ForTheBadge.com/images/badges/uses-html.svg)](http://ForTheBadge.com)
-[![ForTheBadge uses-css](http://ForTheBadge.com/images/badges/uses-css.svg)](http://ForTheBadge.com)
-[![ForTheBadge uses-js](http://ForTheBadge.com/images/badges/uses-js.svg)](http://ForTheBadge.com)
 
-> Piano simples feito para tocar as notas:
-<pre>
-* Dó  - C
-* Ré  - D
-* Mi  - E
-* Fá  - F
-* Sol - G
-* Lá  - A
-* Si  - B
-</pre>
+<p align="center">
+  <img src="./screenshots/1.png">
+</p>
+
+<p align="center">
+  <a href="https://forthebadge.com"> <img src="https://forthebadge.com/images/badges/uses-html.svg" /> </a>
+  <a href="https://forthebadge.com"> <img src="https://forthebadge.com/images/badges/uses-css.svg" /> </a>
+  <a href="https://forthebadge.com"> <img src="https://forthebadge.com/images/badges/uses-js.svg" /> </a>
+</p>
+
+## :scroll: Sobre
+
+Pianinho.
+
+---
+
+## :rocket: Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+---
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,118 +1,33 @@
-// notas
-const mainKeys = document.querySelectorAll(".main-note");
-const subKeys = document.querySelectorAll(".sub-note");
+const audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
+const notes = document.querySelectorAll(".note");
 
-function playNote(event) {
-  //mouse
-  // dó
-  if (event.target == mainKeys[0]) {
-    playTone(261, "sine", 0.5);
-  }
-
-  // dó sustenido
-  else if (event.target == subKeys[0]) {
-    playTone(277.2, "sine", 0.5);
-  }
-
-  // ré
-  else if (event.target == mainKeys[1]) {
-    playTone(293.7, "sine", 0.5);
-  }
-
-  // ré sustenido
-  else if (event.target == subKeys[1]) {
-    playTone(311.1, "sine", 0.5);
-  }
-
-  // mi
-  else if (event.target == mainKeys[2]) {
-    playTone(329.6, "sine", 0.5);
-  }
-
-  // fá
-  else if (event.target == mainKeys[3]) {
-    playTone(349.2, "sine", 0.5);
-  }
-
-  // fá sustenido
-  else if (event.target == subKeys[2]) {
-    playTone(370.0, "sine", 0.5);
-  }
-
-  // sol
-  else if (event.target == mainKeys[4]) {
-    playTone(392.0, "sine", 0.5);
-  }
-
-  // sol sustenido
-  else if (event.target == subKeys[3]) {
-    playTone(466.2, "sine", 0.5);
-  }
-
-  // lá
-  else if (event.target == mainKeys[5]) {
-    playTone(440.0, "sine", 0.5);
-  }
-
-  // lá sustenido
-  else if (event.target == subKeys[4]) {
-    playTone(466.2, "sine", 0.5);
-  }
-
-  // si
-  else if (event.target == mainKeys[6]) {
-    playTone(493.9, "sine", 0.5);
-  }
-
-  //   //teclado
-  //   // dó
-  //   if (event.key == "c") {
-  //     playTone(261, "sine", 0.5);
-  //   }
-
-  //   // ré
-  //   else if (event.key == "d") {
-  //     playTone(293.7, "sine", 0.5);
-  //   }
-
-  //   // mi
-  //   else if (event.key == "e") {
-  //     playTone(329.6, "sine", 0.5);
-  //   }
-
-  //   // fá
-  //   else if (event.key == "f") {
-  //     playTone(349.2, "sine", 0.5);
-  //   }
-
-  //   // sol
-  //   else if (event.key == "g") {
-  //     playTone(392.0, "sine", 0.5);
-  //   }
-
-  //   // lá
-  //   else if (event.key == "a") {
-  //     playTone(440.0, "sine", 0.5);
-  //   }
-
-  //   // si
-  //   else if (event.key == "b") {
-  //     playTone(493.9, "sine", 0.5);
-  //   }
-  else {
-    return 0;
-  }
+const notes_frequency = {
+  "A": 440.0,
+  "A#": 466.16,
+  "B": 493.88,
+  "C": 261.63,
+  "C#": 277.18,
+  "D": 293.66,
+  "D#": 311.13,
+  "E": 329.63,
+  "F": 349.23,
+  "F#": 369.99,
+  "G": 392.0,
+  "G#": 415.3
 }
 
-//clique do mouse
-for (let key of mainKeys) {
-  key.addEventListener("click", playNote);
+const play_note = (e) => {
+  const oscillator = audio_ctx.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = notes_frequency[e.target.getAttribute("data-note")];
+  oscillator.connect(audio_ctx.destination);
+  oscillator.start();
+
+  setTimeout((_) => {
+    oscillator.stop();
+  }, 250);
 }
 
-//clique do mouse
-for (let key of subKeys) {
-  key.addEventListener("click", playNote);
-}
-
-// // clique do teclado
-// window.addEventListener("keydown", playNote);
+notes.forEach((note) => {
+  note.addEventListener("click", play_note);
+});
